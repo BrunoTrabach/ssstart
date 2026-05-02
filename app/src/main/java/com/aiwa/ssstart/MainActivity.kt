4b0679f6c1233211a216ecb1367e1a554915be90
BrunoTrabach <brunomaia.vix@gmail.com>
2026-05-01 22:47:58 -0300

    feat: adiciona MainActivity pro celular + ícone no launcher

--- app/src/main/java/com/aiwa/ssstart/MainActivity.kt
+++ app/src/main/java/com/aiwa/ssstart/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.aiwa.ssstart
+
 import android.app.Activity
 import android.os.Bundle
-import android.view.KeyEvent
+import android.widget.TextView
 
-class MainActivity: Activity() {
-    private val secret = SecretCodeDetector()
-    override fun onCreate(b: Bundle?) { super.onCreate(b); finish() }
-    override fun onKeyDown(k: Int, e: KeyEvent): Boolean {
-        if (secret.onKey(k)) { /* abrir configurações */ }
-        return super.onKeyDown(k, e)
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "SSStart v2.0-dev\n\nApp de TV rodando no celular para teste.\n\nModelo: ${android.os.Build.MODEL}\nTamanho detectado: ${TvSizeDetector.getSize(this)}\""
+        tv.textSize = 18f
+        tv.setPadding(64, 64, 64, 64)
+        setContentView(tv)
     }
 }
